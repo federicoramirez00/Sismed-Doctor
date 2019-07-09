3cb94856233eab37f66f6f939f94bb399c1faf49
federamirez00 <20140050@ricaldone.edu.sv>
2019-07-09 14:15:39 -0600

    app 1.0

--- www/js/init.js
+++ www/js/init.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.tooltipped').tooltip();
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
 });
